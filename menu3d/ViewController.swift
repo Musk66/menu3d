80508343023638ecf5790a0c29eba0958a215aab
tiger <tiger@tiger.local>
2016-04-27 15:53:49 +0800

    Initial Commit

---
+++ menu3d/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  menu3d
+//
+//  Created by tiger on 16/4/27.
+//  Copyright © 2016年 tq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
